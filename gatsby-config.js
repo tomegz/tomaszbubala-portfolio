c64e2122ce00e5724d090f0af2ff72fdec12f4d1
tomegz <tomaszbubala@o2.pl>
2018-01-24 15:20:39 +0100

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
+      },
+    }
+  ],
+};

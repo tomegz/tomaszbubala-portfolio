4bf60b0dee7a0fda04cb6c619b65fa749c543936
tomegz <tomaszbubala@o2.pl>
2018-01-29 16:01:39 +0100

    add projects page

---
+++ src/pages/projects.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Socials from '../components/Socials';
+
+const ProjectsPage = () => (
+  <div className="projects-page">
+    <h1>Projects</h1>
+    <div className="page-desc">
+      <p>You can find some of the projects i was working on in the past months below.
+         Not all of them are commercial, but every one of them is 100% passion.</p>
+    </div>
+    <footer>
+      <Socials />
+    </footer>
+  </div>
+)
+
+export default ProjectsPage;

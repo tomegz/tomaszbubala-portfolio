716e61df73342d102d54810efc994a0640d75be9
tomegz <tomaszbubala@o2.pl>
2018-02-01 20:09:35 +0100

    make projects page responsive

--- src/components/Socials/index.js
+++ src/components/Socials/index.js
@@ -4,22 +4,27 @@ import React from "react";
 import FaGithub from 'react-icons/lib/fa/github';
 import FaStackOverflow from 'react-icons/lib/fa/stack-overflow';
 import FaLinkedIn from 'react-icons/lib/fa/linkedin';
+import FaEnvelope from 'react-icons/lib/fa/envelope'
 
 const Socials = props => {
   const github = "https://github.com/tomegz";
   const linkedin = "https://www.linkedin.com/in/tomasz-buba%C5%82a-55790b145/";
   const stackoverflow = "https://stackoverflow.com/users/7475314/tomasz-buba%c5%82a?tab=profile";
+  const mailto = "mailto:tomaszbubala37@gmail.com";
   return (
     <div className="socials">
       <ul className="social-icons">
         <li>
-          <a href={github}target="_blank"><FaGithub /></a>
+          <a href={github} target="_blank"><FaGithub /></a>
         </li>
         <li>
-          <a href={stackoverflow}target="_blank"><FaStackOverflow /></a>
+          <a href={stackoverflow} target="_blank"><FaStackOverflow /></a>
         </li>
         <li>
-          <a href={linkedin}target="_blank"><FaLinkedIn /></a>
+          <a href={linkedin} target="_blank"><FaLinkedIn /></a>
+        </li>
+        <li>
+          <a href={mailto}><FaEnvelope /></a>
         </li>
       </ul>
     </div>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import Socials from '../components/Socials';
 
+const projects = [
+  {
+    name: "3D Estate",
+    image_url: "/images/project-3destate.png"
+  },
+  {
+    name: "Price tag generator",
+    image_url: "/images/project-ptg.png"
+  },
+  {
+    name: "Scrabblista",
+    image_url: "/images/project-scrabblista.png"
+  }
+];
+
 const ProjectsPage = () => (
   <div className="projects-page">
     <h1>Projects</h1>
     <div className="page-desc">
       <p>You can find some of the projects i was working on in the past months below.
          Not all of them are commercial, but every one of them is 100% passion.</p>
+    </div>
+    <div className="portfolio-wrapper">
+      {projects.map(project => (
+        <div className="portfolio-item">
+          <a href='#' target ="_blank" style={{ textDecoration: "none" }}>
+            <div className="portfolio-item-image" 
+                 style={{ backgroundImage: `url(${project.image_url})`}}>
+            </div>
+            <div className="portfolio-item-title-wrapper">
+              <div className="portfolio-item-title">
+                {project.name}
+              </div>
+            </div>
+          </a>
+        </div>
+      ))}
     </div>
     <footer>
       <Socials />
